1cd0067e8a1540aecf927262e40ca10e15145f0a
jingxi <774874572@qq.com>
2021-08-19 21:32:19 +0800

    deploy

---
+++ precache-manifest.d7a66c55bbb7decd7a7ecda72d3e30cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7937f2b25adeb3b7dd7d7aab3c8fd0b",
+    "url": "/React-PocketBookWebsite/index.html"
+  },
+  {
+    "revision": "f7fd741645d554f1f0bb",
+    "url": "/React-PocketBookWebsite/static/css/main.8a50928f.chunk.css"
+  },
+  {
+    "revision": "c38c83fcac7ec1d52f40",
+    "url": "/React-PocketBookWebsite/static/js/2.dea36588.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/React-PocketBookWebsite/static/js/2.dea36588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7fd741645d554f1f0bb",
+    "url": "/React-PocketBookWebsite/static/js/main.054f590d.chunk.js"
+  },
+  {
+    "revision": "231fcead40a96a3d5740",
+    "url": "/React-PocketBookWebsite/static/js/runtime-main.1ac3c992.js"
+  }
+]);
